0194f390325f6f5ff14ed573db8ef10c11a0d610
olgatorres <bestlaly@mail.ru>
2024-01-13 16:22:17 +0300

    changes in html search and level3

--- recommendations/catalog/forms.py
+++ recommendations/catalog/forms.py
@@ -2,4 +2,4 @@ from django import forms
 
 
 class SearchForm(forms.Form):
-    search_activity = forms.CharField(max_length=255, initial='Введите название активности', label='')
+    search_activity = forms.CharField(max_length=255,  label='Введите название активности')
